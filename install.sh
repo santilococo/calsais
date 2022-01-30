6fa35fe839850297e38ed2718c12512664675e7a
Santiago Lo Coco <santilococo.01@gmail.com>
2022-01-30 17:13:40 -0300

    Add infobox

--- install.sh
+++ install.sh
@@ -290,6 +290,7 @@ getThePackages() {
 installImportantPackages() {
     msg="\nWe will continue with the installation of some important packages in the background. Please press OK and wait."
     dialog --msgbox "$msg" 8 60
+    dialog --infobox "\nUpgrading archlinux-keyring." 5 32
     pacman -S --noconfirm archlinux-keyring 2>&1 | debug
     getThePackages "Y" "installImportantPackages"
     runInChroot "systemctl enable NetworkManager; systemctl enable fstrim.timer" 2>&1 | debug
